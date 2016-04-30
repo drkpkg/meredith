9e78614eb47a2e81ba17f094234587ec7fbe40f3
Drkpkg <daniel.uremix@gmail.com>
2016-04-30 02:08:55 -0400

    API para consumo en movil iniciada.

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,38 @@
 Rails.application.routes.draw do
 
   root 'page#index'
-
-  #Sessions
   get 'login' => 'sessions#login'
-  post 'login/auth'=> 'sessions#authorize_user'
   get 'logout'=> 'sessions#logout'
-  
-  #User
+  get 'signup', to: "users#new_profile"
   get 'me/:id/info' => "users#profile", as: :me_info
   get 'me/:id/edit' => 'users#edit_profile', as: :me_edit
   get 'me/:id' => 'users#dashboard', as: :me
-  
-  get 'signup', to: "users#new_profile"
-  
+  get 'me/:id/events' => 'events#index', as: :event
+  get 'me/:id/events/new_event' => 'events#new_event', as: :event_new
+  get 'me/:id/events/:event_id' => 'events#info_event', as: :event_info
+  get 'me/:id/events/:event_id/edit' => 'events#edit_event', as: :event_edit
+  get '/photos' => 'photos#index'
+  post 'login/auth'=> 'sessions#authorize_user'
   post 'users/create_profile' => 'users#create_profile'
+  post 'event/create_event' => 'events#create_event', as: :event_create
+  post 'event/:id/add_photos' => 'photos#create', as: :photo_create
   patch 'users/update_profile' => 'users#update_profile'
-  delete 'users/destroy_profile' => 'users#destroy_profile'
   patch 'users/block_profile' => 'users#block_profile'
   patch 'users/follow_profile' => 'users#follow_profile'
   patch 'users/suscribe_event' => 'users#suscribe_event'
-
-  get 'me/:id/events' => 'events#index', as: :event
-  get 'me/:id/events/new_event' => 'events#new_event', as: :event_new  
-  get 'me/:id/events/:event_id' => 'events#info_event', as: :event_info
-  get 'me/:id/events/:event_id/edit' => 'events#edit_event', as: :event_edit
-  post 'event/create_event' => 'events#create_event', as: :event_create  
   patch 'event/update_event' => 'events#update_event', as: :event_update
   delete 'event/:event_id/delete_event' => 'events#delete_event', as: :event_delete
-
-  get '/photos' => 'photos#index'
-  post 'event/:id/add_photos' => 'photos#create', as: :photo_create
+  delete 'users/destroy_profile' => 'users#destroy_profile'
   delete 'me/:id/events/:event_id/:image_id/delete_photo' => 'photos#delete', as: :photo_delete
-
   match '404' => 'page#not_found', via: [:get]
+
+  #API for mobile
+  scope 'resources' do
+    scope 'bearone' do
+      post '_auth_user' => 'api#_auth_user'
+      post '_create_user' => 'api#_create_user'
+      get '_info_user' => 'api#_info_user'
+    end
+  end
+
 end
