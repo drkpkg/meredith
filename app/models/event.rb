50eeebaec5443666c7345f37ea15857cff5e4ba2
drkpkg <daniel.uremix@gmail.com>
2016-05-07 04:04:45 -0400

    Cambios en galerias.

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,7 +12,18 @@ class Event
   validates_uniqueness_of :event_name, sensitive: false, message: "El nombre de la galería debe ser único"
   validates_presence_of :event_name, message: "El nombre de la galería no puede estar vacío"
   
+  #before_create :set_gallery_folder
+  
   def total_photos
     Photo.where(event_id: self.id).count
+  end
+  
+  def set_gallery_folder
+    FileUtils.mkdir("#{Rails.root}/public/files/photos/#{self.id}")    
+  end
+  
+  def delete_folder
+    FileUtils.rm_rf("#{Rails.root}/public/files/photos/#{self.id}")
+    FileUtils.rm_rf("#{Rails.root}/public/files/photos/#{self.id}")
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,9 +7,8 @@ class Photo
   field :description, type: String
   field :likes, type: Array, default: []
   has_mongoid_attached_file :image_original,
-                            :url => "/files/:class/:attachment/:id/:style/:basename.:extension",
-                            :path => ":rails_root/public/files/:class/:attachment/:id/:style/:basename.:extension" ,
-                            :preserve_files => "false"
+                            :url => "/files/:class/:event_id/:attachment/:id/:style/:basename.:extension",
+                            :path => ":rails_root/public/files/:class/:event_id/:attachment/:id/:style/:basename.:extension"
 
   has_mongoid_attached_file :image_processed,
                             processors: [:watermark],
@@ -20,16 +19,15 @@ class Photo
                                 original: {geometry: '60%',watermark_path: "#{Rails.root}/public/images/logo.gif", position: "Center"}
                             },
                             :url => "/files/:class/:attachment/:id/:style/:basename.:extension",
-                            :path => ":rails_root/public/files/:class/:attachment/:id/:style/:basename.:extension",
-                            :preserve_files => "false"
+                            :path => ":rails_root/public/files/:class/:attachment/:id/:style/:basename.:extension"
 
   validates_attachment_size :image_original,
                             :in => 0.megabytes..500.megabytes,
                             message: "El tamaño del archivo es muy grande"
 
   validates_attachment_file_name :image_original,
-                                 matches: [/png\Z/, /jpe?g\Z/, /iiq\Z/, /IIQ\Z/],
-                                 message: "Formato no válido de imagen"
+                                 matches: [/png\Z/, /jpe?g\Z/],
+                                 message: "Formato no admitido"
 
   validates_attachment_content_type :image_original,
                                     content_type: /\Aimage/,
@@ -37,20 +35,26 @@ class Photo
 
   belongs_to :event
 
-  before_destroy :delete_all_shit
+  before_destroy :delete_photo_data
 
   def has_likes
     return 0 if self.likes.empty?
-    self.likes.lenght
+    return self.likes.lenght
   end
   
-  def original_size
+  def original_resolution
     Paperclip::Geometry.from_file(self.image_original.path)
   end
 
-  def delete_all_shit
-    File.delete(Rails.root + "public/files/photos/original/#{self.id}/*")
-    File.delete(Rails.root + "public/files/photos/processed/#{self.id}/*")
+  def original_file_size_in_kb
+    self.image_original_file_size/1000 
   end
 
+  private
+
+  def delete_photo_data
+    FileUtils.rm_rf("#{Rails.root}/public/files/photos/image_originals/#{self.id}")
+    FileUtils.rm_rf("#{Rails.root}/public/files/photos/image_processeds/#{self.id}")
+  end
+  
 end
